a51cd191e1ea9541755d51652b49d01233006f99
David Munkacsi <david.munkacsi@campus.tu-berlin.de>
2018-06-25 22:54:50 +0200

    Bootstrapper is no more. Initialization is in ContractApi.

--- application/src/api/ContractApi.js
+++ application/src/api/ContractApi.js
@@ -1,6 +1,22 @@
 import Web3 from 'web3';
-import Contracts from '../bootstrapper';
-import { DbApi } from '../api/DbApi';
+
+var apartmentContractJson = require('../contracts-json/Apartment.json');
+var apartmentABI=apartmentContractJson.abi;
+for(var appKey in apartmentContractJson.networks){
+    var apartmentKey = appKey;
+}
+let apartmentAddress=apartmentContractJson.networks[apartmentKey].address;
+
+var userContractJson = require('../contracts-json/User.json');
+var userABI = userContractJson.abi;
+for(var uKey in userContractJson.networks){
+    var userKey = uKey;
+}
+let userAddress=userContractJson.networks[userKey].address;
+
+const web3=new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const ApartmentContract = web3.eth.contract(apartmentABI).at(apartmentAddress);
+const UserContract = web3.eth.contract(userABI).at(userAddress);
 
 const getAccounts = () => {
     const web3=new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
@@ -14,7 +30,7 @@ const createUser = (address, password) => {
         const transactionObject = {
             from: address
         }
-        Contracts.User.createUserPasswordMapping.sendTransaction(password, transactionObject, (error, result) => {
+        UserContract.createUserPasswordMapping.sendTransaction(password, transactionObject, (error, result) => {
             if(!error) {
                 console.log(result);
             } else {

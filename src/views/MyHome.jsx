5277d33315cdc2ae8b0504bba205684090223272
David Munkacsi <david.munkacsi@campus.tu-berlin.de>
2018-06-26 23:33:09 +0200

    Node Express added.

--- src/views/MyHome.jsx
+++ src/views/MyHome.jsx
@@ -17,10 +17,24 @@ export class MyHomeView extends React.Component {
     this.handleChange = this.handleChange.bind(this);
   }
 
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
   handleChange(evt) {
     this.setState({ [evt.target.name]: evt.target.value });
   }
-
 
   render() {
     return (

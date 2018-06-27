fdf6604332ed60b8abce3a15b66ece587a7eef47
David Munkacsi <david.munkacsi@campus.tu-berlin.de>
2018-06-27 22:23:57 +0200

    MongoDB WIP. React and node are working together.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,57 @@
 const express = require('express');
-const dbApi = require('./MongoDbApi');
+const mongoClient = require('mongodb').MongoClient;
+const dbName = "smartrent";
+const uri = "mongodb://SmartRent:berlin28041993@smartrentdb-shard-00-00-estgp.mongodb.net:27017,smartrentdb-shard-00-01-estgp.mongodb.net:27017,smartrentdb-shard-00-02-estgp.mongodb.net:27017/test?ssl=true&replicaSet=SmartRentDB-shard-0&authSource=admin&retryWrites=true";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/connect', (req, res) => {
-  var result = dbApi.connect();
-  res.send({ res: result });
+app.get('/api/getAccountCount', (req, res) => {
+  mongoClient.connect(uri, (err, db) => {
+    if (err) 
+      res.send( { success: false, message: "Connection with the database could not be establised." });
+    var dbo = db.db(dbName);
+    dbo.collection("accounts").count((err, count) => {
+        if (err) 
+          res.send( { success: false, message: "Connection with the database could not be establised." });
+        else {
+          res.send({ success: true, count: count });
+        }
+        db.close();
+      });
+  });
+});
+
+app.post('api/createUser', (req, res) => {
+  mongoClient.connect(uri, function(err, db) {
+    if (err)
+      res.send( { success: false, message: "Connection with the database could not be establised." });
+    var dbo = db.db(dbName);
+    dbo.collection("users").insertOne(user, function(err, res) {
+        if (err) {
+          res.send( { success: false, message: "User could not be inserted." });
+        } else {
+          res.send( { success: true, message: "User inserted." });
+        }
+        db.close();
+    });
+  });
+});
+
+app.post('api/createAccounts', (req, res) => {
+  mongoClient.connect(uri, (err, db) => {
+      if (err) 
+        res.send( { success: false, message: "Connection with the database could not be establised." });
+      var dbo = db.db(dbName);
+      dbo.collection("accounts").insertOne(addresses, (err, res) => {
+          if (err) {
+            res.send( { success: false, message: "Accounts could not be inserted." });
+          } else {
+            res.send( { success: true, message: "Number of accounts inserted: " + res.insertedCount });
+          }
+          db.close();
+      });
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/api/DbApi.js
+++ src/api/DbApi.js
@@ -5,9 +5,7 @@ var db = new Dexie("smartRentDb");
 
 const initializeDb = () => {
   const schema = {
-    accounts: '++id,address',
-    users: '++id,name,address',
-    currentUser: 'id,name,address'
+    currentUser: '++id,name,address'
   }
   db.version(1).stores(schema);
   initializeAccounts();
@@ -21,64 +19,20 @@ const initializeDb = () => {
 
 const initializeAccounts = () => {
   let accounts = ContractApi.getAccounts();
-  db.users.toCollection().count().then((cntUsers) => {
-    db.accounts.toCollection().count().then((cntAccounts) => {
-      if (cntUsers === 0 && cntAccounts === 0) {
-        console.log("Initializing accounts...");
-        accounts.forEach((acc) => {
-          db.accounts.add({address: acc})
-        });
-      }
-    })
-  });
+  const response = fetch('/api/getAccountCount');
+  console.log(response);
+  // const body = response.json();
+  // if (response.status !== 200) throw Error(body.message);
+
 }
 
-const getDbAccounts = () => {
-  const accounts = [];
-  db.accounts.toArray().then((a) => {
-      a.forEach(acc => {
-        accounts.push(acc);
-      })
-  });
-  return accounts;
-}
-
-const getDbUsers = () => {
-  const users = [];
-  db.users.toArray().then((u) => {
-    u.forEach(user => {
-      users.push(user);
-    })
-  });
-  return users;
-}
-
-const getDbUser = (username) => {
-  return db.users.get({ name: username });
-}
-
-const createDbUser = (username, password) => {
-  return db.accounts.toCollection().first().then(account => {
-    if (account == null) {
-      throw new Error("No more available accounts.");
-    }
-    db.accounts.delete(account.id).then(() => {
-      db.users.add({name: username, address: account.address}).then((id) => {
-        db.users.get(id).then(user => {
-          ContractApi.createUser(user.address, password)
-        });
-      });
-    });
-  });
+const createCurrentUser = (user) => {
+  return db.users.add({name: user.username, address: user.address});
 }
 
 const DbApi = {
   initializeDb: initializeDb,
-  initializeAccounts: initializeAccounts,
-  getDbAccounts: getDbAccounts,
-  getDbUser: getDbUser,
-  getDbUsers: getDbUsers,
-  createDbUser: createDbUser
+  createCurrentUser: createCurrentUser
 }
 
 export default DbApi;
